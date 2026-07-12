9d146d3f118048c94cdf18ce705c140487aa4518
arshdeep68083-cyber <arshdeep68083@gmail.com>
2026-07-12 08:13:34 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
 
 // Firebase Config
 const firebaseConfig = {
@@ -16,6 +17,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Auth & Firestore
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Services
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Export
+export { auth, db, storage };
